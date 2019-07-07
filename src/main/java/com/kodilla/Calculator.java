a163fd8a7862ed2b121a099f9aff3e720df767a9
naros1 <sorankasim@gmail.com>
2019-07-07 18:12:06 +0200

    FirstCommit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
     private int a;
     private int b;
 
+
     public Calculator(int a, int b){
         this.a = a;
         this.b = b;
@@ -15,5 +16,9 @@ public class Calculator {
     public int subtract(){
         return a-b;
     }
-
+    public static void main(String args[]){
+        Calculator toCalculate = new Calculator(3,5);
+        System.out.println(toCalculate.sum());
+        System.out.println(toCalculate.subtract());
+    }
 }
